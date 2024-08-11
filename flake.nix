8364808d982e4845a27f916a12c874ed0316dfda
uonr <dev@yuru.me>
2024-08-11 23:24:35 +0900

    Add flake.nix

---
+++ flake.nix
@@ -0,0 +1,31 @@
+{
+  description = "FATE";
+
+  inputs = {
+    flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "nixpkgs/nixos-unstable";
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShells.default =
+          with pkgs;
+          mkShell {
+            buildInputs = [
+              nodejs
+              yarn
+            ];
+          };
+      }
+    );
+}
